9b063aeb2ab51a77c1eb0b33685c995d67a14681
gmanojbabu148 <gmanojbabu1999@gmail.com>
2021-05-02 19:09:48 +0530

    Create a React app and publish it to GitHub Pages

---
+++ src/Person/Person.js
@@ -0,0 +1,82 @@
+import React, { useState, useEffect } from 'react';
+import './Person.css';
+import loadingGif from './Scan-Text-Loading-Label.gif';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import { Button, OverlayTrigger, Popover } from 'react-bootstrap';
+import PersonDetails from './PersonDetails';
+
+const Person = () => {
+    const [personsState, setPersonsState] = useState([]);
+
+    const hasPerson = personsState.length > 0
+    useEffect(() => {
+        fetch('https://608ce77d9f42b20017c3e5d5.mockapi.io/studentdList')
+            .then((res) => res.json())
+            .then(personArray => {
+                const newPersonsState = personArray.map((person) => {
+                    return person
+                })
+                setPersonsState(newPersonsState)
+            })
+    }, [])
+
+    return (
+        <div>
+            {hasPerson ?
+                <PersonDetails personsState={personsState} /> : <img className="loadingGif" src={loadingGif} alt="Loading..."></img>}
+        </div >
+    )
+
+
+}
+
+export default Person;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+ // return (
+    //     <div>
+    //         {hasPerson ?
+    //             <div className="ListRow">
+    //                 <h1 className="App">Persons List</h1>
+    //                 {personsState.map((person) => {
+    //                     return <div key={person.id} className="ListColumn">
+    //                         <div className="ListCard">
+    //                             <p><span class="text-secondary">Name: </span>{person.name}</p>
+    //                             <p><span class="text-secondary">Parents Name: </span> {person.parentsName}</p>
+    //                             <PersonDetails
+    //                                 createdAt={person.createdAt}
+    //                                 dOB={person.dOB}
+    //                                 placeOfBirthState={person.placeOfBirthState}
+    //                                 class={person.class}
+    //                                 isStudentAverageIsGood={person.isStudentAverageIsGood}
+    //                                 studentLocationLat={person.studentLocationLat}
+    //                                 studentLocationLong={person.studentLocationLong}
+    //                                 placeOfBirthCity={person.placeOfBirthCity}
+    //                                 motherName={person.motherName}
+    //                                 fatherName={person.fatherName}
+    //                                 fatherPhoneNumber={person.fatherPhoneNumber}
+    //                                 studentProfileUrl={person.studentProfileUrl}
+    //                             />
+    //                         </div>
+    //                     </div>
+    //                 })}
+    //             </div> : <img className="loadingGif" src={loadingGif} alt="Loading..."></img>}
+    //     </div >
+    // )
---
+++ src/Person/PersonDetails.js
@@ -0,0 +1,69 @@
+import React from 'react';
+import './Person.css';
+import { Button, OverlayTrigger, Popover } from 'react-bootstrap';
+
+const popover = (props) => {
+    return (<Popover id="popover-basic">
+        <Popover.Title as="h3">Details</Popover.Title>
+        <Popover.Content>
+            <h6><span class="text-secondary">createdAt: </span>{props.createdAt}</h6>
+            <h6><span class="text-secondary">dOB: </span>{props.dOB}</h6>
+            <h6><span class="text-secondary">placeOfBirthState: </span>{props.placeOfBirthState}</h6>
+            <h6><span class="text-secondary">class: </span>{props.class}</h6>
+            <h6><span class="text-secondary">isStudentAverageIsGood: </span>{props.isStudentAverageIsGood ? "Yes" : "No"}</h6>
+            <h6><span class="text-secondary">studentLocationLat: </span>{props.studentLocationLat}</h6>
+            <h6><span class="text-secondary">studentLocationLong: </span>{props.studentLocationLong}</h6>
+            <h6><span class="text-secondary">placeOfBirthCity: </span>{props.placeOfBirthCity}</h6>
+            <h6><span class="text-secondary">motherName: </span>{props.motherName}</h6>
+            <h6><span class="text-secondary">fatherName: </span>{props.fatherName}</h6>
+            <h6><span class="text-secondary">fatherPhoneNumber: </span>{props.fatherPhoneNumber}</h6>
+            <h6><span class="text-secondary">studentProfileUrl: </span> <a href={props.studentProfileUrl}>{props.studentProfileUrl}</a></h6>
+        </Popover.Content>
+    </Popover>)
+}
+
+const PersonDetails = (props) => {
+
+    { console.log(props) }
+    return (
+        <div className="ListRow">
+            <h1 className="App">Persons List</h1>
+            {props.personsState.map((person) => {
+                return <div key={person.id} className="ListColumn">
+                    <div className="ListCard">
+                        <p><span class="text-secondary">Name: </span>{person.name}</p>
+                        <p><span class="text-secondary">Parents Name: </span> {person.parentsName}</p>
+                        <OverlayTrigger trigger="click" placement="bottom"   overlay={popover(person)}  >
+                            <Button variant="outline-secondary">More</Button>
+                        </OverlayTrigger>
+                    </div>
+                </div>
+            })}
+
+        </div>
+    )
+}
+
+export default PersonDetails;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// const PersonDetails = (props) => {
+//     return (
+//         <OverlayTrigger trigger="click" placement="bottom" overlay={popover(props)}     >
+//             <Button variant="outline-secondary">More</Button>
+//         </OverlayTrigger>
+//     )
+// }
+
